d60a5b3127c289627f038b6848984df01cad8408
Jonathan McKinnon <150212666+JohnnyMcKinnon@users.noreply.github.com>
2025-06-24 01:32:01 +0000

    code should be implemented. As of rn, this is a basis if my output is wrong.

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -1,16 +1,18 @@
-public class DoubleLinkedList implements Comparable<nfs_DoubleLinkedList> {
+public class DoubleLinkedList implements Comparable<DoubleLinkedList> {
 
     private static final String DEFAULT_NAME = "Whatever";
 
     private String name;
     private Node head;
     private Node tail;
+    private int size;
 
     /** Basic constructor */
     public DoubleLinkedList(String name) {
         this.name = name;
         this.head = null;
         this.tail = null;
+        this.size = 0;
     } // Basic constructor
 
     /** Default constructor passes default name to basic constructor */
@@ -22,25 +24,57 @@ public class DoubleLinkedList implements Comparable<nfs_DoubleLinkedList> {
     // --------
     // * well, almost always ...
     public String toString() {
-        return this.name;
+        return this.name + " (size=" + size + ")";
     } // method toString
 
     /** Add a new node to the linked list */
     public void add(Node node) {
-        if (this.head == null) {
-            this.head = node;
-        } else {
-            this.tail.setNext(node);
+        if (node == null) {
+            return; // nothing to add
         }
-        this.tail = node;
+        if (this.head == null) { // first node
+            this.head = node;
+            this.tail = node;
+        } else { // add to tail
+            this.tail.setNext(node);
+            node.setPrevious(this.tail); // maintains previous pointer
+            this.tail = node;
+        }
+        size++; // update count
     } // method add
 
     // overload method add to add a node by value
     public void add(String value) {
+        add(new Node(value));
     } // method add
 
+    //return current nodes
+    public int size() {
+        return this.size;
+    } // method size
+
     // implement the comparable interface
-    public int compareTo(nfs_DoubleLinkedList other) {
-        return -1234567;
+    public int compareTo(DoubleLinkedList other) {
+        if (other == null) {
+            return 1; // a non‑null list is "greater" than null
+        }
+        else{
+        return Integer.compare(this.size, other.size());
     } // method compareTo
+
+    //Return index of the first node with value equal to given string, or −1 if no such node exists.
+    public int indexOf(String value) {
+        int index = 0;
+        for (Node current = head; current != null; current = current.getNext()) {
+            if (current.getValue().equals(value)) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    } // method indexOf
+    //Return true if the list contains a node whose value equals the given string.
+    public boolean contains(String value) {
+        return indexOf(value) != -1;
+    } // method contains
 } // class DoubleLinkedList
